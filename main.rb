9b7ef75ea7c3ab4efd5c81cda06be6db762145d2
Alex Lemay <alemay8@hotmail.ca>
2024-02-02 13:39:55 -0500

    Add the program

---
+++ main.rb
@@ -0,0 +1,12 @@
+def bubble_sort(array)
+  n = array.length
+  (0...n).each do |i|
+    (0...n - i - 1).each do |j|
+      if array[j] > array[j+1]
+        array[j],array[j+1] = array[j+1],array[j]
+      end
+    end
+  end
+  array
+end
+bubble_sort([4,3,78,2,0,2])
